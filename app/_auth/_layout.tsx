46909ffc46ce395bbbf29f35bb7c061a7090f8b4
Ken Española <revas4192@gmail.com>
2026-02-01 21:49:04 +0800

    Create _layout.tsx

---
+++ app/_auth/_layout.tsx
@@ -0,0 +1,19 @@
+
+import { Stack } from "expo-router";
+import { useTheme } from "@/theme/useTheme";
+
+export default function AuthLayout() {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: theme.surface },
+      }}
+    >
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
+  );
+}
