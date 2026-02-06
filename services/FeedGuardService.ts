22493774c35217e92efb667f70175437206e37be
Ken Española <revas4192@gmail.com>
2026-02-06 14:36:05 +0800

    Update FeedGuardService.ts

--- services/FeedGuardService.ts
+++ services/FeedGuardService.ts
@@ -4,7 +4,6 @@ import { DEVICE_CONFIG } from "../config/deviceConfig";
 export async function canDispenseFood(): Promise<boolean> {
   const weight = await getCurrentWeight();
 
-  return (
-    weight <= DEVICE_CONFIG.WEIGHT.EMPTY_THRESHOLD
-  );
+  
+  return weight <= (DEVICE_CONFIG.WEIGHT.EMPTY_THRESHOLD || 10);
 }
